f83a5d344a08a1531cea4be1d3801c7f8eb73275
Fred-QS <in-touch-fg@quanticalsolutions.com>
2020-07-11 22:40:12 +0200

    dataseeder

--- src/dataSeeder.php
+++ src/dataSeeder.php
@@ -1,16 +1,20 @@
 <?php
 
-$seed = (object) [
-    'description' => 'The Quantic framework represents the new advance of Quantical Solutions in terms of innovation.
+namespace Quantic\Igniter\Seeder;
+
+class Dataseeder
+{
+    protected $seed = (object) [
+        'description' => 'The Quantic framework represents the new advance of Quantical Solutions in terms of innovation.
 It is made up of different development modules as well as DevOps technologies like Docker. We find PHP 7.4, MySQL, React, React Native, Node.js, Webpack ...
 The goal of Quantic is to facilitate cross-platform and multi-device development work. A solution for managing web app, mobile app and APIs.
 A multi-layered solution bringing together the latest technologies, in their version or their reputation, in order to offer a new generation of digital solutions.',
 
-    'email' => 'infos@quanticalsolutions.com',
+        'email' => 'infos@quanticalsolutions.com',
 
-    'logged' => true,
+        'logged' => true,
 
-    'charts' => 'Mix and match bar and line charts to provide a clear visual distinction between datasets.
+        'charts' => 'Mix and match bar and line charts to provide a clear visual distinction between datasets.
     Plot complex, sparse datasets on date time, logarithmic or even entirely custom scales with ease.
     Out of the box stunning transitions when changing data, updating colours and adding datasets.
     Chart.js is a community maintained project, contributions welcome!
@@ -18,15 +22,21 @@ A multi-layered solution bringing together the latest technologies, in their ver
     Great rendering performance across all modern browsers (IE11+).
     Redraws charts on window resize for perfect scale granularity.',
 
-    'owl' => 'Fully Customisable - Over 60 options. Easy for novice users and even more powerful for advanced developers.
+        'owl' => 'Fully Customisable - Over 60 options. Easy for novice users and even more powerful for advanced developers.
     Touch and Drag Support - Designed specially to boost mobile browsing experience. Mouse drag works great on desktop too!
     Fully Responsive - Almost all options are responsive and include very intuitive breakpoints settings.
     Modern Browsers - Owl uses hardware acceleration with CSS3 Translate3d transitions. Its fast and works like a charm!
     Zombie Browsers - CSS2 fallback supported for older browser.
     Modules and Plugins - Owl Carousel supports plugin modular structure. Therefore, you can detach plugins that you won\'t use on your project or create new ones that fit your needs',
 
-    'fullcalendar' => 'Powerful and Lightweight - Has over 100 customizable settings. Built as separate modules to keep filesize down.
+        'fullcalendar' => 'Powerful and Lightweight - Has over 100 customizable settings. Built as separate modules to keep filesize down.
     Open Source - All code is open source and hosted on GitHub. ',
-    
-    'docker' => 'Developing apps today requires so much more than writing code. Multiple languages, frameworks, architectures, and discontinuous interfaces between tools for each lifecycle stage creates enormous complexity. Docker simplifies and accelerates your workflow, while giving developers the freedom to innovate with their choice of tools, application stacks, and deployment environments for each project.'
-];
+
+        'docker' => 'Developing apps today requires so much more than writing code. Multiple languages, frameworks, architectures, and discontinuous interfaces between tools for each lifecycle stage creates enormous complexity. Docker simplifies and accelerates your workflow, while giving developers the freedom to innovate with their choice of tools, application stacks, and deployment environments for each project.'
+    ];
+
+    public function Seeder()
+    {
+        return $this->seed;
+    }
+}
