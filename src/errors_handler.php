5d0651a29f2b1ae2e8bc95e970a7a44f6df8ae61
Fred-QS <in-touch-fg@quanticalsolutions.com>
2020-07-11 18:56:45 +0200

    errors handler

---
+++ src/errors_handler.php
@@ -0,0 +1,7 @@
+<?php
+
+ini_set('error_prepend_string',"<div class='errorPhp'><p><span style='color: white; width: 100%;'>Erreur <b style='color: white;'>PHP</b> de type</span>");
+ini_set('error_append_string',"</p></div><script src='https://" . $_SERVER['SERVER_NAME'] . "/assets/js/errors.js'></script>");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(-1);
