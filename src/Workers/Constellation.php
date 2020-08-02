cf42504c5bd9a8f4f29a282af01678752b59d0e5
Fred-QS <in-touch-fg@quanticalsolutions.com>
2020-08-02 13:31:27 +0200

    Constellations

--- src/Workers/Constellation.php
+++ src/Workers/Constellation.php
@@ -4,20 +4,26 @@ namespace Quantic\Igniter\Workers;
 
 use Quantic\Igniter\Workers\Request;
 
-class Constellation extends Request
+class Constellation
 {
-    public static function group()
+    public static function group($array, $callback)
     {
 
     }
 
-    public static function get()
+    public static function get($uri, $array)
     {
+        $type = Request::all();
+        if ($type[1] != 'POST' || $type[1] == 'GET') {
 
+        }
     }
 
-    public static function post()
+    public static function post($uri, $array)
     {
+        $type = Request::all();
+        if ($type[1] == 'POST') {
 
+        }
     }
 }
