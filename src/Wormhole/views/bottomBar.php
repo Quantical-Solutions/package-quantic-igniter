5c738469ded7fc76f28a8113d468174607e61e9e
Fred-QS <in-touch-fg@quanticalsolutions.com>
2020-08-06 01:03:13 +0200

    Wormhole

---
+++ src/Wormhole/views/bottomBar.php
@@ -0,0 +1,5 @@
+<div id="wormhole">
+    <style><?php echo file_get_contents('./assets/style.css') ?></style>
+    Wormhole
+    <script><?php echo file_get_contents('./assets/script.js') ?></script>
+</div>
