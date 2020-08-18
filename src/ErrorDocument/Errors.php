9b35063ea6e35dc65f1cebd22e8dabe82cfc3868
Fred-QS <in-touch-fg@quanticalsolutions.com>
2020-08-19 01:08:48 +0200

    update

--- src/ErrorDocument/Errors.php
+++ src/ErrorDocument/Errors.php
@@ -5,38 +5,24 @@ $code = $_SERVER['REDIRECT_STATUS'];
 $error = 'Unknown error';
 
 $codes = [
+    200 => 'Not Found',
     403 => 'Forbidden',
     404 => 'Not Found',
-    500 => 'Internal Server Error'
+    500 => 'Server Error'
 ];
 
 $source_url = 'http'.((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') ? 's' : '').'://'.$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
+$type = 200;
+$message = 'All\'s good';
+
 if (array_key_exists($code, $codes) && is_numeric($code)) {
 
-    $error =  "$code | {$codes[$code]}";
+    if ($code == 200) {
+        $type = 404;
+    } else {
+        $type = $code;
+    }
+
+    $message =  $codes[$code];
 }
-
-?>
-
-<!DOCTYPE html>
-<html lang="fr-FR">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
-    <meta name="referrer" content="origin-when-cross-origin">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="msapplication-tap-highlight" content="no" />
-    <title>Quantic - $error</title>
-    <link rel="icon" href="/dimension/media/img/favicon.png">
-    <link rel="stylesheet" href="/dimension/dist/styles.css" type="text/css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-</head>
-<body>
-<div style="width: 100%; height: 100vh; display: flex; justify-content: center; align-items: center;">
-    <h1><?= $error ?></h1>
-</div>
-<script type="text/javascript" src="/dimension/dist/scripts.js" ></script>
-</body>
-</html>
--- src/ErrorDocument/assets/script.js
+++ src/ErrorDocument/assets/script.js
@@ -4,4 +4,37 @@
 * ============================================================================
 */
 
-console.log('Error...');
+getErrorType();
+
+function getErrorType() {
+
+    type = document.querySelector('body').dataset.type;
+    message = '';
+    explain = '';
+
+    switch (type) {
+
+        case '200':
+            message = 'All\'s good.';
+            explain = 'Everything\'s rollin\' right so what are you doin\' here...?';
+            break;
+
+        case '403':
+            message = 'Forbiden access.';
+            explain = 'You don\'t permission to access this folder/file.';
+            break;
+
+        case '404':
+            message = 'Page not found.';
+            explain = 'Are you sure about your request ?';
+            break;
+
+        case '500':
+            message = 'Internal Server error.';
+            explain = 'Seems like your server met script parsing troubles...';
+            break;
+    }
+
+    console.clear('test');
+    console.log('🚫 %c' + type + ' Error -> ' + message + ' %c' + explain, 'color:#ff531a; font-weight:bold;', 'color:inherit');
+}
